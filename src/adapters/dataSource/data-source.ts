69d15a82c24f2e57b6da1948039edbde0fb1d0f2
Jean F <157065253+JeanFabioGruber@users.noreply.github.com>
2025-03-16 18:41:20 -0300

    Add files via upload

---
+++ src/adapters/dataSource/data-source.ts
@@ -0,0 +1,31 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+
+import * as dotenv from "dotenv";
+
+
+dotenv.config();
+
+const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE, NODE_ENV } =
+  process.env;
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: DB_HOST,
+  port: parseInt(DB_PORT || "5432"),
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,  
+  entities: [
+    `${__dirname}/**/entity/*.{ts,js}`
+],
+migrations: [
+    `${__dirname}/**/migrations/*.{ts,js}`
+],
+  synchronize: false,
+  logging: false,
+});
+
+console.log("Entidades carregadas:", AppDataSource.options.entities);
+console.log("Migrations carregadas:", AppDataSource.options.migrations);
+
